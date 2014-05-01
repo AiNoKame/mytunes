662de07fc9e368222d8bda07e8672bc56e9dede3
Hack Reactor Students <Rod.Calumpong@gmail.com>
2014-04-30 18:10:15 -0700

    sonqueue is starting to come together

--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -4,6 +4,10 @@ var SongModel = Backbone.Model.extend({
   play: function(){
     // Triggering an event here will also trigger the event on the collection
     this.trigger('play', this);
+  },
+
+  enqueue: function() {console.log("queued!");
+    this.trigger('enqueue', this);
   }
 
 });
